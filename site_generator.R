ba56a0219658934cc2ebab37e259ca1d1431f5bd
SimonHanzal <2263391h@student.gla.ac.uk>
2020-11-23 17:13:13 +0000

    Pavel's recommendation

---
+++ site_generator.R
@@ -0,0 +1,8 @@
+library("metathis")
+
+meta_tag("description" = "A postgraduate blog and webpage with research in cognitive psychology.")
+meta_tag("author" = "Simon Hanzal")
+meta_tag("robots" = "index, follow")
+meta_tag("googlebot" = "index, follow")
+meta_tag("keywords" = "psychology, Hanzal, Glasgow, fatigue, cognitive, research, attention"
+)
