eaa23a705a500ba5e5a14255860697ae988f1e25
Simon Hanzal <71489526+SimonHanzal@users.noreply.github.com>
2023-07-13 18:43:52 -0400

    Delete site_generator.R

--- site_generator.R
+++
@@ -1,8 +0,0 @@
-library("metathis")
-
-meta_tag("description" = "A postgraduate blog and webpage with research in cognitive psychology.")
-meta_tag("author" = "Simon Hanzal")
-meta_tag("robots" = "index, follow")
-meta_tag("googlebot" = "index, follow")
-meta_tag("keywords" = "psychology, Hanzal, Glasgow, fatigue, cognitive, research, attention"
-)
